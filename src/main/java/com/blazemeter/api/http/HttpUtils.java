134386d5a35c967c8eea55abacc32647493aa703
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-16 13:48:58 +0300

    add okhttp3 dependency

--- src/main/java/com/blazemeter/api/http/HttpUtils.java
+++ src/main/java/com/blazemeter/api/http/HttpUtils.java
@@ -1,15 +1,51 @@
+/**
+ * Copyright 2017 BlazeMeter Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.blazemeter.api.http;
 
 import com.blazemeter.api.logging.Logger;
 import net.sf.json.JSON;
 import net.sf.json.JSONObject;
+import okhttp3.Authenticator;
+import okhttp3.Credentials;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.Route;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Class for working with HTTP requests
  */
 public class HttpUtils {
+
+    public static final String PROXY_HOST = "http.proxyHost";
+    public static final String PROXY_PORT = "http.proxyPort";
+    public static final String PROXY_USER = "http.proxyUser";
+    public static final String PROXY_PASS = "http.proxyPass";
+
+    protected static final String ACCEPT = "Accept";
+    protected static final String APP_JSON = "application/json";
+    protected static final String CONTENT_TYPE = "Content-type";
+    protected static final String APP_JSON_UTF_8 = "application/json; charset=UTF-8";
+    protected static final String AUTHORIZATION = "Authorization";
 
     protected final static int TIMEOUT = 5;
 
@@ -17,15 +53,40 @@ public class HttpUtils {
 
     protected final String address;
     protected final String dataAddress;
+    private OkHttpClient httpClient;
 
     public HttpUtils(String address, String dataAddress, Logger logger) {
         this.address = address;
         this.dataAddress = dataAddress;
         this.logger = logger;
-//        this.httpClient = createHTTPClient();
+        this.httpClient = createHTTPClient();
+    }
+
+    private JSONObject executeGetRequest(String url) throws IOException {
+        return execute(createRequestBuilder(url).get().build());
+    }
+
+    private JSONObject executePostRequest(String url, RequestBody requestBody) throws IOException {
+        return execute(createRequestBuilder(url).post(requestBody).build());
+    }
+
+    private JSONObject execute(Request request) throws IOException {
+        return JSONObject.fromObject(executeRequest(request));
+    }
+
+    private Request.Builder createRequestBuilder(String url) {
+        final Request.Builder builder = new Request.Builder().url(url).
+                addHeader(ACCEPT, APP_JSON).
+                addHeader(CONTENT_TYPE, APP_JSON_UTF_8);
+        return addRequiredHeader(builder);
     }
 
 
+    private String executeRequest(Request request) throws IOException {
+        String response = httpClient.newCall(request).execute().body().string();
+        logger.debug("Received response: " + response);
+        return response;
+    }
     /**
      * Create Get Request
      */
@@ -67,6 +128,15 @@ public class HttpUtils {
         return null;
     }
 
+    protected String extractErrorMessage(String response) {
+        return response;
+    }
+
+    protected Request.Builder addRequiredHeader(Request.Builder requestBuilder) {
+        // NOOP
+        return requestBuilder;
+    }
+
     public Logger getLogger() {
         return logger;
     }
@@ -77,5 +147,62 @@ public class HttpUtils {
 
     public String getDataAddress() {
         return dataAddress;
+    }
+
+    private OkHttpClient createHTTPClient() {
+        Proxy proxy = Proxy.NO_PROXY;
+        Authenticator auth = Authenticator.NONE;
+        try {
+            String proxyHost = System.getProperty(PROXY_HOST);
+            if (!StringUtils.isBlank(proxyHost)) {
+                logger.info("Using http.proxyHost = " + proxyHost);
+                int proxyPort = getProxyPort();
+                proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+                auth = createAuthenticator();
+            }
+
+            return new OkHttpClient.Builder()
+                    .addInterceptor(new RetryInterceptor(logger))
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .proxy(proxy)
+                    .proxyAuthenticator(auth).build();
+        } catch (Exception ex) {
+            logger.warn("ERROR Instantiating HTTPClient. Exception received: ", ex);
+            throw new RuntimeException("ERROR Instantiating HTTPClient. Exception received: ", ex);
+        }
+    }
+
+    private int getProxyPort() {
+        try {
+            int proxyPort = Integer.parseInt(System.getProperty(PROXY_PORT));
+            logger.info("Using http.proxyPort = " + proxyPort);
+            return proxyPort;
+        } catch (NumberFormatException nfe) {
+            logger.warn("Failed to read http.proxyPort: ", nfe);
+            return 8080;
+        }
+    }
+
+    private Authenticator createAuthenticator() {
+        final String proxyUser = System.getProperty(PROXY_USER);
+        logger.info("Using http.proxyUser = " + proxyUser);
+        final String proxyPass = System.getProperty(PROXY_PASS);
+        logger.info("Using http.proxyPass = " + StringUtils.left(proxyPass, 4));
+        if (!StringUtils.isBlank(proxyUser) && !StringUtils.isBlank(proxyPass)) {
+            return new Authenticator() {
+                @Override
+                public Request authenticate(Route route, Response response) throws IOException {
+                    String credential = Credentials.basic(proxyUser, proxyPass);
+                    if (response.request().header("Proxy-Authorization") != null) {
+                        return null; // Give up, we've already attempted to authenticate.
+                    }
+                    return response.request().newBuilder()
+                            .header("Proxy-Authorization", credential)
+                            .build();
+                }
+            };
+        }
+        return Authenticator.NONE;
     }
 }
