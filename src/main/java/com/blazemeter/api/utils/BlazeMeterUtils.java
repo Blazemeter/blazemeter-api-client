c1c576b46522df6a43d7163af3d85b423d258884
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-16 15:52:04 +0300

    fix bzm utils

--- src/main/java/com/blazemeter/api/utils/BlazeMeterUtils.java
+++ src/main/java/com/blazemeter/api/utils/BlazeMeterUtils.java
@@ -17,30 +17,36 @@ package com.blazemeter.api.utils;
 import com.blazemeter.api.http.HttpUtils;
 import com.blazemeter.api.logging.Logger;
 import com.blazemeter.api.logging.UserNotifier;
-import org.apache.commons.codec.binary.Base64;
+import net.sf.json.JSON;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+import net.sf.json.JsonConfig;
+import okhttp3.Credentials;
+import okhttp3.Request;
+import org.apache.commons.lang.StringUtils;
 
 
 public class BlazeMeterUtils extends HttpUtils {
 
+    public static final String EMPTY_TOKEN = "";
     private final String token;
 
     protected final UserNotifier notifier;
 
     /**
-     * @param apiKeyId - BlazeMeter Api Key Id
+     * @param apiKeyId     - BlazeMeter Api Key Id
      * @param apiKeySecret - BlazeMeter Api Key Secret
-     * @param address - BlazeMeter app address: http://a.blazemeter.com/
-     * @param dataAddress - BlazeMeter data address: http://data.blazemeter.com/
-     * @param notifier - user notifier, to show user information
-     * @param logger - logger, for log events of http requests / response etc.
+     * @param address      - BlazeMeter app address: http://a.blazemeter.com/
+     * @param dataAddress  - BlazeMeter data address: http://data.blazemeter.com/
+     * @param notifier     - user notifier, to show user information
+     * @param logger       - logger, for log events of http requests / response etc.
      */
     public BlazeMeterUtils(String apiKeyId, String apiKeySecret,
                            String address, String dataAddress,
                            UserNotifier notifier, Logger logger) {
         super(address, dataAddress, logger);
-        // TODO: replace this other lib
-        // TODO: add check for empty tokens.
-        this.token = new String(Base64.encodeBase64((apiKeyId + ':' + apiKeySecret).getBytes()));
+        this.token = isValidCredantials(apiKeyId, apiKeySecret) ? Credentials.basic(apiKeyId, apiKeySecret) : EMPTY_TOKEN;
         this.notifier = notifier;
     }
 
@@ -49,29 +55,31 @@ public class BlazeMeterUtils extends HttpUtils {
         this("", "", address, dataAddress, notifier, logger);
     }
 
-//    @Override
-//    protected void addRequiredHeader(HttpRequestBase httpRequestBase) {
-//        if (token != null && !token.isEmpty()) {
-//            httpRequestBase.setHeader("Authorization", "Basic " + new String(Base64.encodeBase64(token.getBytes())));
-//        }
-//    }
+    protected boolean isValidCredantials(String apiKeyId, String apiKeySecret) {
+        return !StringUtils.isBlank(apiKeyId) && !StringUtils.isBlank(apiKeySecret);
+    }
 
-//    @Override
-//    protected String extractErrorMessage(String response) {
-//        if (response != null && !response.isEmpty()) {
-//            try {
-//                JSON jsonResponse = JSONSerializer.toJSON(response, new JsonConfig());
-//                if (jsonResponse instanceof JSONObject) {
-//                    JSONObject object = (JSONObject) jsonResponse;
-//                    JSONObject errorObj = object.getJSONObject("error");
-//                    if (errorObj.containsKey("message")) {
-//                        return errorObj.getString("message");
-//                    }
-//                }
-//            } catch (JSONException ex) {
-//                log.debug("Cannot parse JSON error response: " + response);
-//            }
-//        }
-//        return response;
-//    }
+    @Override
+    protected Request.Builder addRequiredHeader(Request.Builder requestBuilder) {
+        return EMPTY_TOKEN.equals(token) ? requestBuilder : requestBuilder.addHeader(AUTHORIZATION, token);
+    }
+
+    @Override
+    protected String extractErrorMessage(String response) {
+        if (response != null && !response.isEmpty()) {
+            try {
+                JSON jsonResponse = JSONSerializer.toJSON(response, new JsonConfig());
+                if (jsonResponse instanceof JSONObject) {
+                    JSONObject object = (JSONObject) jsonResponse;
+                    JSONObject errorObj = object.getJSONObject("error");
+                    if (errorObj.containsKey("message")) {
+                        return errorObj.getString("message");
+                    }
+                }
+            } catch (JSONException ex) {
+                logger.debug("Cannot parse JSON error response: " + response);
+            }
+        }
+        return response;
+    }
 }
