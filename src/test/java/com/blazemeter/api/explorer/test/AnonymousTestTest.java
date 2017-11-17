e9f4f099f58f178af9c185b1eef59e3de2dd5926
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-17 17:48:10 +0300

    add AnonymousTestTest

---
+++ src/test/java/com/blazemeter/api/explorer/test/AnonymousTestTest.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2017 BlazeMeter Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazemeter.api.explorer.test;
+
+import com.blazemeter.api.explorer.Master;
+import com.blazemeter.api.logging.Logger;
+import com.blazemeter.api.logging.LoggerTest;
+import com.blazemeter.api.logging.UserNotifier;
+import com.blazemeter.api.logging.UserNotifierTest;
+import com.blazemeter.api.utils.BlazeMeterUtilsEmul;
+import net.sf.json.JSONObject;
+
+import static com.blazemeter.api.utils.BlazeMeterUtilsEmul.BZM_ADDRESS;
+import static com.blazemeter.api.utils.BlazeMeterUtilsEmul.BZM_DATA_ADDRESS;
+import static org.junit.Assert.*;
+
+public class AnonymousTestTest {
+    @org.junit.Test
+    public void testFlow() throws Exception {
+        Logger logger = new LoggerTest();
+        UserNotifier notifier = new UserNotifierTest();
+
+        BlazeMeterUtilsEmul emul = new BlazeMeterUtilsEmul(BZM_ADDRESS, BZM_DATA_ADDRESS, notifier, logger);
+
+        JSONObject response = new JSONObject();
+        response.put("result", generateResponse());
+
+
+        AnonymousTest test = new AnonymousTest(emul);
+        emul.addEmul(response.toString());
+        test.startExternal();
+        assertEquals(1, emul.getRequests().size());
+        assertEquals("Request{method=POST, url=http://a.blazemeter.com/api/v4/sessions, tag=null}", emul.getRequests().get(0));
+        checkTest(test);
+
+        try {
+            test.start();
+            fail("Cannot start this test type");
+        } catch (UnsupportedOperationException ex) {
+            assertEquals("Start is not supported for this test type", ex.getMessage());
+        }
+    }
+
+    private JSONObject generateResponse() {
+        JSONObject testResponse = new JSONObject();
+        testResponse.put("id", "responseTestId");
+        testResponse.put("name", "responseTestName");
+
+        JSONObject masterResponse = new JSONObject();
+        masterResponse.put("id", "responseMasterId");
+        masterResponse.put("name", "responseMasterName");
+
+        JSONObject result = new JSONObject();
+        result.put("test", testResponse);
+        result.put("signature", "responseSignature");
+        result.put("master", masterResponse);
+        return result;
+    }
+
+
+    private void checkTest(AnonymousTest test) {
+        Master master = test.getMaster();
+        assertEquals("responseMasterId", master.getId());
+        assertEquals("responseMasterName", master.getName());
+
+        assertEquals("responseSignature", test.getSignature());
+    }
+
+}
