d43ede018d1567de5ea9812d7a758191fdabde39
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-21 17:37:37 +0300

    refactor master

--- src/main/java/com/blazemeter/api/explorer/Master.java
+++ src/main/java/com/blazemeter/api/explorer/Master.java
@@ -28,8 +28,6 @@ import java.util.List;
 
 public class Master extends BZAObject {
 
-    private String MASTER_ID = "/api/v4/masters/{id}";
-
     public Master(BlazeMeterUtils utils, String id, String name) {
         super(utils, id, name);
     }
@@ -37,12 +35,12 @@ public class Master extends BZAObject {
     /**
      * @return public link to the report
      */
-    public String publicreport() throws IOException {
+    public String getPublicReport() throws IOException {
+        logger.info("Get link to public report for master id=" + getId());
         String uri = utils.getAddress() + String.format("/api/v4/masters/%s/public-token", encode(getId()));
         JSONObject obj = new JSONObject();
         obj.put("publicToken", "None");
         JSONObject response = utils.execute(utils.createPost(uri, obj.toString()));
-
         return utils.getAddress() + String.format("/app/?public-token=%s#/masters/%s/summary",
                 extractPublicToken(response.getJSONObject("result")), getId());
     }
@@ -50,15 +48,17 @@ public class Master extends BZAObject {
     /**
      * @return junit report as a string
      */
-    public String junitReport() throws IOException {
+    public String getJUnitReport() throws IOException {
+        logger.info("Get JUnit report for master id=" + getId());
         String uri = utils.getAddress() + String.format("/api/v4/masters/%s/reports/thresholds?format=junit", encode(getId()));
         return utils.executeRequest(utils.createGet(uri));
     }
 
     /**
-     * @return junit report as a string
+     * @return list of Sessions Id
      */
-    public List<String> sessionIds() throws IOException {
+    public List<String> getSessions() throws IOException {
+        logger.info("Get list of sessions for master id=" + getId());
         String uri = utils.getAddress() + String.format("/api/v4/masters/%s/sessions", encode(getId()));
         List<String> sessionIds = new ArrayList<>();
         JSONObject response = utils.execute(utils.createGet(uri));
@@ -71,20 +71,15 @@ public class Master extends BZAObject {
         return sessionIds;
     }
 
-    /**
-     * @return JSONObject
-     */
-
     public JSONArray stop() throws IOException {
+        logger.info("Stop master id=" + getId());
         String uri = utils.getAddress() + String.format("/api/v4/masters/%s/stop", encode(getId()));
         RequestBody emptyBody = RequestBody.create(null, new byte[0]);
         return utils.execute(utils.createPost(uri, emptyBody)).getJSONArray("result");
     }
 
-    /**
-     * @return JSONObject
-     */
     public JSONArray terminate() throws IOException {
+        logger.info("Terminate master id=" + getId());
         String uri = utils.getAddress() + String.format("/api/v4/masters/%s/terminate", encode(getId()));
         RequestBody emptyBody = RequestBody.create(null, new byte[0]);
         return utils.execute(utils.createPost(uri, emptyBody)).getJSONArray("result");
@@ -93,7 +88,8 @@ public class Master extends BZAObject {
     /**
      * @return master status code
      */
-    public int status() throws IOException {
+    public int getStatus() throws IOException {
+        logger.info("Get master status id=" + getId());
         String uri = utils.getAddress() + String.format("/api/v4/masters/%s/status?events=false", encode(getId()));
         JSONObject r = utils.execute(utils.createGet(uri)).getJSONObject("result");
         return r.getInt("progress");
@@ -102,19 +98,20 @@ public class Master extends BZAObject {
     /**
      * @return summary as JSON object
      */
-    public JSONObject summary() throws IOException {
+    public JSONObject getSummary() throws IOException {
+        logger.info("Get summary for master id=" + getId());
         String uri = utils.getAddress() + String.format("/api/v4/masters/%s/reports/main/summary", encode(getId()));
         JSONObject r = utils.execute(utils.createGet(uri)).getJSONObject("result");
         JSONObject sumserv = r.getJSONArray("summary").getJSONObject(0);
-        JSONObject summary = extractSummary(sumserv);
-        return summary;
+        return extractSummary(sumserv);
     }
 
     /**
      * @return functional report as JSON object
      */
-    public JSONObject funcReport() throws IOException {
-        String uri = utils.getAddress() + String.format(MASTER_ID, encode(getId()));
+    public JSONObject getFunctionalReport() throws IOException {
+        logger.info("Get functional report for master id=" + getId());
+        String uri = utils.getAddress() + String.format("/api/v4/masters/%s", encode(getId()));
         JSONObject r = utils.execute(utils.createGet(uri)).getJSONObject("result");
         return r.has("functionalSummary") ? r.getJSONObject("functionalSummary") : new JSONObject();
     }
@@ -122,11 +119,11 @@ public class Master extends BZAObject {
     /**
      * @return note which was applied to master if request was successful
      */
-    public String note(String note) throws IOException {
-        String uri = utils.getAddress() + String.format(MASTER_ID, encode(getId()));
+    public String postNotes(String note) throws IOException {
+        logger.info("Post notes to master id=" + getId());
+        String uri = utils.getAddress() + String.format("/api/v4/masters/%s", encode(getId()));
         String noteEsc = StringEscapeUtils.escapeJson("{'note':'" + note + "'}");
         RequestBody body = RequestBody.create(MediaType.parse("text/plain; charset=ISO-8859-1"), noteEsc);
-
         JSONObject r = utils.execute(utils.createPatch(uri, body)).getJSONObject("result");
         return r.getString("note");
     }
@@ -134,7 +131,8 @@ public class Master extends BZAObject {
     /**
      * @return ci-status as JSONObject
      */
-    public JSONObject cistatus() throws IOException {
+    public JSONObject getCIStatus() throws IOException {
+        logger.info("Get CI status for master id=" + getId());
         String uri = utils.getAddress() + String.format("/api/v4/masters/%s/ci-status", encode(getId()));
         return utils.execute(utils.createGet(uri)).getJSONObject("result");
     }
