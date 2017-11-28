66dfdd7fe2ee7ecb9045992634b7296f0b886183
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-28 13:04:44 +0300

    fix codacy

--- src/test/java/com/blazemeter/ciworkflow/CiBuildTest.java
+++ src/test/java/com/blazemeter/ciworkflow/CiBuildTest.java
@@ -18,7 +18,6 @@ import org.junit.Test;
 import static com.blazemeter.api.utils.BlazeMeterUtilsEmul.BZM_ADDRESS;
 import static com.blazemeter.api.utils.BlazeMeterUtilsEmul.BZM_DATA_ADDRESS;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
