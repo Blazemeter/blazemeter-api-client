2d97913c61cd579726416cf79b13f0738987611f
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-28 12:15:38 +0300

    simplify set emul in CiBuild Test

--- src/test/java/com/blazemeter/ciworkflow/CiBuildTest.java
+++ src/test/java/com/blazemeter/ciworkflow/CiBuildTest.java
@@ -1,13 +1,15 @@
 package com.blazemeter.ciworkflow;
 
 import com.blazemeter.api.explorer.Master;
+import com.blazemeter.api.explorer.MasterTest;
+import com.blazemeter.api.explorer.SessionTest;
 import com.blazemeter.api.explorer.test.AbstractTest;
 import com.blazemeter.api.explorer.test.SingleTest;
+import com.blazemeter.api.explorer.test.SingleTestTest;
 import com.blazemeter.api.logging.LoggerTest;
 import com.blazemeter.api.logging.UserNotifier;
 import com.blazemeter.api.logging.UserNotifierTest;
 import com.blazemeter.api.utils.BlazeMeterUtilsEmul;
-import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
@@ -75,7 +77,7 @@ public class CiBuildTest {
         assertEquals("Request{method=POST, url=http://a.blazemeter.com/api/v4/tests/id/start, tag=null}", emul.getRequests().get(0));
         assertEquals("Request{method=POST, url=http://a.blazemeter.com/api/v4/masters/responseMasterId/public-token, tag=null}", emul.getRequests().get(1));
         assertEquals("http://a.blazemeter.com/app/?public-token=x1x1x1x1x1x1x1x11x1x1x1#/masters/responseMasterId/summary",
-                ciBuild.publicReport);
+                ciBuild.getPublicReport());
         assertEquals("Request{method=PATCH, url=http://a.blazemeter.com/api/v4/masters/responseMasterId, tag=null}", emul.getRequests().get(2));
         assertEquals("Request{method=GET, url=http://a.blazemeter.com/api/v4/masters/responseMasterId/sessions, tag=null}", emul.getRequests().get(3));
         assertEquals("Request{method=POST, url=http://a.blazemeter.com/api/v4/sessions/r-v3-1234567890qwerty/properties?target=all, tag=null}", emul.getRequests().get(4));
@@ -99,83 +101,22 @@ public class CiBuildTest {
         assertTrue(logs.contains("Response: {\"result\":{\"progress\":70}}"));
         assertTrue(logs.contains("Response: {\"result\":{\"progress\":140}}"));
         assertEquals(16, emul.getRequests().size());
-        assertEquals(3230, logger.getLogs().length());
+        assertEquals(logger.getLogs().toString(), 3226, logger.getLogs().length());
     }
 
 
     private void setEmulator(BlazeMeterUtilsEmul emul) {
-        JSONObject testResponse = new JSONObject();
-        testResponse.put("id", "responseTestId");
-        testResponse.put("name", "responseTestName");
-
-        JSONObject masterResponse = new JSONObject();
-        masterResponse.put("id", "responseMasterId");
-        masterResponse.put("name", "responseMasterName");
-        JSONObject response = new JSONObject();
-        response.put("result", masterResponse);
-
-
-        emul.addEmul(response.toString());
+        emul.addEmul(SingleTestTest.generateResponseStartSingleTest());
 
         String token = "x1x1x1x1x1x1x1x11x1x1x1";
-        JSONObject publicToken = new JSONObject();
-        publicToken.put("publicToken", token);
+        emul.addEmul(MasterTest.generateResponseGetPublicToken(token));
+        emul.addEmul(MasterTest.generateResponsePostNote());
+        emul.addEmul(MasterTest.generateResponseGetSessions());
+        emul.addEmul(SessionTest.generateResponsePostProperties());
 
-        JSONObject result = new JSONObject();
-        result.put("result", publicToken);
-        emul.addEmul(result.toString());
-
-
-        result = new JSONObject();
-        result.put("note", "valid");
-
-        response = new JSONObject();
-        response.put("result", result);
-        emul.addEmul(response.toString());
-
-        JSONObject session = new JSONObject();
-        session.put("id", "r-v3-1234567890qwerty");
-        session.put("name", "11");
-        session.put("userId", "12");
-        session.put("testId", "13");
-
-        JSONArray sessionsa = new JSONArray();
-        sessionsa.add(session);
-
-        JSONObject sessions = new JSONObject();
-        sessions.put("sessions", sessionsa);
-
-        result = new JSONObject();
-        result.put("result", sessions);
-        emul.addEmul(result.toString());
-
-
-        result = new JSONObject();
-        result.put("key", "URL");
-        result.put("value", "www.google.com");
-        response.put("result", result);
-        emul.addEmul(response.toString());
-
-        result = new JSONObject();
-        response = new JSONObject();
-
-        result.put("progress", 70);
-        response.put("result", result);
-        emul.addEmul(response.toString());
-
-        result = new JSONObject();
-        result.put("progress", 140);
-
-        response.put("result", result);
-        emul.addEmul(response.toString());
-
-
-        JSONObject status = new JSONObject();
-        status.put("masterId", "id");
-
-        result = new JSONObject();
-        result.put("result", status);
-        emul.addEmul(result.toString());
+        emul.addEmul(MasterTest.generateResponseGetStatus(70));
+        emul.addEmul(MasterTest.generateResponseGetStatus(140));
+        emul.addEmul(MasterTest.generateResponseGetCIStatus());
     }
 
     @Test
