48c82e2d73cf409e99b6fbd190e5d1c7a3f3f659
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-12-05 17:36:21 +0300

    fix Account test

--- src/test/java/com/blazemeter/api/explorer/AccountTest.java
+++ src/test/java/com/blazemeter/api/explorer/AccountTest.java
@@ -79,7 +79,7 @@ public class AccountTest {
         }
         assertEquals("Request{method=GET, url=http://a.blazemeter.com/api/v4/workspaces?accountId=777&enabled=true&limit=1000, tag=null}", emul.getRequests().get(0));
         String logs = logger.getLogs().toString();
-        assertEquals(logs, 274, logs.length());
+        assertEquals(logs, 275, logs.length());
         assertTrue(logs, logs.contains("Get list of workspaces for account id=777"));
     }
 
